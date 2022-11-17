0cb515fb8044fc40c80f8fa9f5848ce22441b2cb
Monica Masae <monica.w.masae@gmail.com>
2022-11-17 12:17:39 +0300

    initial commit, add tailwind and images

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "index.html",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        mercury: '#dce2df',
+        deep_teal: '#0d3632'
+      }
+    
+    },
+  },
+  plugins: [],
+}
